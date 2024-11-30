13d2c71f9dc86f9c9f6dba3b98b886ff979dc0fd
Moses Asiago <mozzeago@gmail.com>
2024-11-30 15:42:25 +0300

    added migrations code

---
+++ database/migrations/2024_11_30_122952_create_tenants_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tenants', function (Blueprint $table) {
+            $table->id(); // INT primary key
+            $table->ulid('tenant_ulid')->unique(); // ULID for tenant identification
+            $table->string('name')->unique(); // Tenant name
+            $table->string('email')->unique(); // Contact email
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tenants');
+    }
+};
---
+++ database/migrations/2024_11_30_123459_create_role_user_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('role_user', function (Blueprint $table) {
+            $table->id(); // INT primary key
+            $table->foreignId('tenant_id')->constrained('tenants')->onDelete('cascade'); // Tenant ID FK
+            $table->foreignId('role_id')->constrained()->onDelete('cascade'); // Role ID
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // User ID
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('role_user');
+    }
+};
