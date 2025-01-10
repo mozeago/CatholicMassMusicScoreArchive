fe2938ff061c3a6666eab7f4b46e47b02059e829
Moses Asiago <mozzeago@gmail.com>
2025-01-11 02:01:58 +0300

    migrations

---
+++ database/migrations/2025_01_10_225557_create_tenants_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTenantsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('tenants', function (Blueprint $table) {
+            $table->id(); // INT primary key
+            $table->ulid('ulid')->unique(); // ULID column for unique identification
+            $table->string('name'); // Tenant name
+            $table->string('email')->unique(); // Tenant email for contact
+            $table->string('domain')->unique()->nullable(); // Tenant's custom domain (optional)
+            $table->string('logo')->nullable(); // Tenant's logo path
+            $table->timestamps(); // Created and updated timestamps
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('tenants');
+    }
+}
